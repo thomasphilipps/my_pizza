78a7283cece497f7fb9c38a1c1c5ccff40a0231b
Thomas Philipps <thomas.philipps@gmail.com>
2023-06-22 22:37:04 +0200

    My new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'tito': {
+        'ingredients': {'gorgonzola': 2, 'basil': 1, 'ham': 2},
+        'price': 11,
     }
 }
